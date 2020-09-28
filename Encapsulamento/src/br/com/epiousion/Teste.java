daef3a6dbd7e3cd639d04176a24526e333ce3817
Vinicius <vinioliveira015@gmail.com>
2020-09-27 22:53:39 -0300

    Teste.java
    This class runs the 'Aluno' class

---
+++ Encapsulamento/src/br/com/epiousion/Teste.java
@@ -0,0 +1,17 @@
+package br.com.epiousion;
+
+public class Teste {
+
+	public static void main(String[] args) {
+
+		Aluno a = new Aluno();
+		a.setIdade(17);
+
+		a.setNome("Artioli");
+		
+		System.out.println(a.getIdade);
+		
+		
+	}
+
+}
