c4197a85ba988eb8000e22aae06480d5f5b45148
Vinicius <vinioliveira015@gmail.com>
2020-09-27 22:56:29 -0300

    This class is responsible for running the 'Calculo' class.

---
+++ Sobrecarga/src/br/edu/uniso/Main.java
@@ -0,0 +1,16 @@
+package br.edu.uniso;
+
+public class Main {
+
+	public static void main(String[] args) {
+
+		Calculo c = new Calculo();
+		System.out.println(c.soma(3, 4, 5));
+		System.out.println(c.soma(1, 2));
+		System.out.println(c.soma(3, 4, 5.7f));
+
+		
+
+	}
+
+}
