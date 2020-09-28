6ffa79a254ec99f3dd0d00e8d5bacd2776b90920
Vinicius <vinioliveira015@gmail.com>
2020-09-27 22:47:49 -0300

    This class is used to run the class 'Aluno'

---
+++ Construtores/src/br/edu/uniso/Main.java
@@ -0,0 +1,19 @@
+package br.edu.uniso;
+
+public class Main {
+
+	public static void main(String[] args) {
+
+		Aluno a = new Aluno();
+		Aluno b = new Aluno();
+		Aluno c = new Aluno();
+
+		
+		System.out.println(a.getContador());
+		System.out.println(b.getContador());
+		System.out.println(c.getContador());
+		
+		
+	}
+
+}
