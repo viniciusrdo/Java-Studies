2a517caac26b70dc2868a1679c84f5085adc2160
Vinicius <vinioliveira015@gmail.com>
2020-09-27 22:55:50 -0300

    this class is an application of methods overload, and its resources

---
+++ Sobrecarga/src/br/edu/uniso/Calculo.java
@@ -0,0 +1,16 @@
+package br.edu.uniso;
+
+public class Calculo {
+
+	public int soma(int a, int b){
+		return a+b;
+	}
+	
+	public int soma(int a, int b, float c){
+		return (int)(a+b+c);
+	}
+	
+	public int soma(int a, int b, int c){
+		return a+b+c;
+	}
+}
